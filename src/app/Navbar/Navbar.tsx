df12b6fd89bcb465267c942fc9f911bb9f9febc1
Jamal Gregg <102499266+MaidenlessEldenLord@users.noreply.github.com>
2024-06-14 11:00:20 -0400

    Create Navbar.tsx

---
+++ src/app/Navbar/Navbar.tsx
@@ -0,0 +1,50 @@
+import { getCart } from "@/lib/db/cart";
+import Image from "next/image";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+import ShoppingCartButton from "./ShoppingCartButton";
+import UserMenuButton from "./UserMenuButton";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { getServerSession } from "next-auth";
+import tshirt from "../../assets/tshirt.jpg";
+
+async function searchProducts(formData: FormData) {
+    "use server";
+
+    const searchQuery = formData.get("searchQuery")?.toString();
+
+    if (searchQuery) {
+        redirect("/search?query=" + searchQuery);
+    }
+}
+
+export default async function Navbar() {
+    const cart = await getCart();
+    const session = await getServerSession(authOptions);
+
+    return(
+        <div className="bg-base-100">
+            <div className="navbar max-w-7xl m-auto flex-col sm:flex-row gap-2">
+                <div className="flex-1">
+                    { <Link href="/" className="btn btn-ghost text-xl normal-case">
+                        <Image src={tshirt} alt="logo"/>
+                        Thread Kount
+                    </Link> }
+                </div>
+                <div className="flex-none gap-2">
+                    <form action={searchProducts}>
+                        <div className="form-control">
+                            <input 
+                                name="searchQuery"
+                                placeholder="Search" 
+                                className="input input-bordered w-full min-w-[100px]" 
+                            />
+                        </div>
+                    </form>
+                    <ShoppingCartButton cart={cart}/>
+                    <UserMenuButton session={session}/>
+                </div>
+            </div>
+        </div>
+    )
+}
